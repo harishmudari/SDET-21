740c0d51c966607df1e074b1fa8f3ab9c31bff1c
harish479 <harishmudari@gmail.com>
2021-09-23 13:01:43 +0530

    intitial commit

---
+++ Vtiger/src/test/java/com/vtiger/contact/ContactTest.java
@@ -0,0 +1,11 @@
+package com.vtiger.contact;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+	public void createContactTest() {
+		System.out.println("createContactTest..!! message");
+	}
+
+}
